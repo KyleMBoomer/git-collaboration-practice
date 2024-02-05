17631b99c263cd219339b20b4fbd20a2d48a5033
“Kyle <kylemboomer@gmail.com>
2024-02-05 12:38:33 -0600

    Add Java Basics

---
+++ fun-with-java.js
@@ -0,0 +1,6 @@
+//two forward slashes creates comments
+//to declare variables, we use "let, const, and var"
+var stuff = 'activities' //string
+var age = 39 //number
+var married = true //boolean
+//I assume a lot of this is similar in Ruby?
