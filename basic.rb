6764c1ec94066db212aebae2e1c1566eaa1f0f1a
Grant Davis <grantdavis303@gmail.com>
2024-02-06 22:12:36 -0700

    Basic Ruby Stuff

---
+++ basic.rb
@@ -0,0 +1,32 @@
+
+# Hello, welcome to Ruby! This is a comment that can't see by the compiler.
+# There are four basic data types in Ruby, and they are:
+
+intro_string = "This is a string!"
+this_is_an_integer = 51     # whole number
+this_is_a_float = 125.12    # decimal
+this_is_a_bool = false
+
+# When you want to call a variable, you just use type the name of the variable, like:
+
+intro_string
+this_is_an_integer
+this_is_a_float
+this_is_a_bool
+
+# Now, to run a program in Ruby, you need to type `ruby [name_of_file].rb` in the terminal 
+# in the folder where the file lives. For example, to run this file, you would type: `ruby basic.rb`
+
+# Try to run it now.
+
+# If you run the file, you will notice that nothing appears in the terminal. Why is that?
+# To see any code in the terminal, you have to use 'puts' in front of the variable(s) you want to see.
+
+# Delete the '=begin' and run the program again.
+
+=begin 
+
+puts intro_string
+puts this_is_an_integer
+puts this_is_a_float
+puts this_is_a_bool
